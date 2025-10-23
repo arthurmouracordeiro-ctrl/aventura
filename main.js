f11bddd003eb120abfc881739e106c4e1fc21501
arthurmouracordeiro-ctrl <arthur.moura.cordeiro@escola.pr.gov.br>
2025-10-23 10:28:45 -0300

    d

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert('TESTE')
+const avanca = document.querySelectorAll('.btn-proximo')
+console.log(avanca)
+
+avanca.forEach
+button.addEventListener('click',function(){
+ const atual = docuument.querySelector('.ativo')
+ const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+ 
+ atual.classList.remove('ativo')
